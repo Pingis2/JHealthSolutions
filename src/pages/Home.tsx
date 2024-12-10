0d85072b0ec34a52dbff8dfd2a5786824792a2be
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-12-10 12:00:07 +0100

    Added - styling header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import logo from "../assets/logo/JHealthSolutions-logo.png";
+import "../styling/home.scss";
 
 export const Home = () => {
   return (
-    <div>
-      <header>
+    <div className="home-container">
+      <header className="home-header">
         <p>JHealthSolutions</p>
         <img
           src={logo}
