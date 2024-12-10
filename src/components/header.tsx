9ea6ff263f3ddb1908ab7c1fdb5d82ed31ee1c57
Pingis2 <anton@worksit.se>
2024-12-10 13:40:26 +0100

    added icons and header, as well as routing logic

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,31 @@
+import { useNavigate } from 'react-router-dom';
 import '../assets/logo/oranta-logo.png';
 
 export const Header = () => {
+
+    const navigate = useNavigate();
+
+    const handleNavigation = (path: string) => {
+        navigate(path);
+    }
+
     return (
         <>
             <header className='header'>
-                <img src="../src/assets/logo/oranta-logo.png" alt="" className='header-logo'></img>
+                <img src="../src/assets/logo/oranta-logo.png" alt="logo of oranta" className='header-logo'></img>
                 <nav>
                     <ul className="nav-items">
-                        <li>Home</li>
-                        <li>Science & Technology</li>
-                        <li>Collaboration Partners</li>
-                        <li>Career</li>
-                        <li>Contact</li>
+                        <li onClick={() => handleNavigation('/')}>Home</li>
+                        <li onClick={() => handleNavigation('/science-technology')}>Science & Technology</li>
+                        <li onClick={() => handleNavigation('/collaboration-partners')}>Collaboration Partners</li>
+                        <li onClick={() => handleNavigation('/career')}>Career</li>
+                        <li onClick={() => handleNavigation('/contact')}>Contact</li>
+                        <li>
+                            <img src='../src/assets/icons/uk-flag-icon.png' alt='icon of the uk flag' className='language-icon' />
+                        </li>
                     </ul>
                 </nav>
+                
             </header>
         </>
     )
