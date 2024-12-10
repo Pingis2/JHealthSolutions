0ffa5664b387a912ab7883f688f1ad2edf89b490
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-12-10 10:39:15 +0100

    Added - header structure

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,18 @@
+import logo from "../assets/logo/JHealthSolutions-logo.png";
+
 export const Home = () => {
-  return <>Home</>;
+  return (
+    <div>
+      <header>
+        <p>JHealthSolutions</p>
+        <img
+          src={logo}
+          alt="JHealthSolutions logo"
+          className="jhealt-solutions-logo"
+        />
+      </header>
+    </div>
+  );
 };
 
 export default Home;
