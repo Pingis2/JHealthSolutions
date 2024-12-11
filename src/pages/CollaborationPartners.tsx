1b77f8f2b600924e4e93711ea42250c46c15269a
Pingis2 <anton@worksit.se>
2024-12-11 10:19:51 +0100

    added images and text

--- src/pages/CollaborationPartners.tsx
+++ src/pages/CollaborationPartners.tsx
@@ -1,8 +1,24 @@
-import { Header } from "../components/header";
-
 export const CollaborationPartners = () => {
   return <>
-    <Header></Header>
+    <div className="profiles">
+      <section className="profile-jessica">
+        <img src="../src/assets/images/collaboration-partners/jessica-image.png" alt="image of Jessica Bräun" loading="lazy"></img>
+        <div className="info-text">
+          <h1>Jessica Bräun</h1>
+          <h3>Dipl. Ing. (FH) Biotechnologie</h3>
+          <h3>Founder JHealthSolutions GmbHG</h3>
+        </div>
+      </section>
+
+      <section className="profile-dmytro">
+        <img src="../src/assets/images/collaboration-partners/dmytro-image.png" alt="image of Jessica Bräun" loading="lazy"></img>
+        <div className="info-text">
+          <h1>Dmytro Vakulenko</h1>
+          <h3>Prof. Dr. Biological Sciences</h3>
+          <h3>Founder Innovation in Medicine LLC</h3>
+        </div>
+      </section>
+    </div>
   </>;
 };
 
