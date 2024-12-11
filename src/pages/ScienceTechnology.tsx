3ca5cb28c6744bcff5829edfd550d199f5d54f20
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-12-11 12:40:04 +0100

    Added - styling for mobile

--- src/pages/ScienceTechnology.tsx
+++ src/pages/ScienceTechnology.tsx
@@ -1,9 +1,38 @@
 import { Header } from "../components/header";
+import scienceImage from "../assets/images/home-page/science-image.jpg";
+import { useNavigate } from "react-router-dom";
 
 export const ScienceTechnology = () => {
-  return <>
-    <Header></Header>
-  </>;
+  const navigate = useNavigate();
+
+  const handleNavigation = (path: string) => {
+    navigate(path);
+  };
+
+  return (
+    <div science-technology-container>
+      <header className="green-header">
+        <div className="header-content">
+          <ul className="header-list">
+            <li onClick={() => handleNavigation("/")} className="home-link">
+              Home
+            </li>
+            <li className="page-title">Science & Technology</li>
+          </ul>
+        </div>
+      </header>
+
+      <Header></Header>
+
+      <div className="image-container">
+        <img
+          src={scienceImage}
+          alt="Science and Technology"
+          className="science-technology-image"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ScienceTechnology;
