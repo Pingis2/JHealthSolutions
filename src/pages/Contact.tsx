f6ae8a30bd0ec751c57697c59046643386bdfe31
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-12-12 09:47:54 +0100

    Added - green header

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,31 @@
+import { useNavigate } from "react-router-dom";
 import { Header } from "../components/header";
 
 export const Contact = () => {
-  return <>
-    <Header></Header>
-  </>;
+  const navigate = useNavigate();
+
+  const handleNavigation = (path: string) => {
+    navigate(path);
+  };
+
+  return (
+    <div>
+      <header className="green-header">
+        <div className="header-content">
+          <ul className="header-list">
+            <li onClick={() => handleNavigation("/")} className="home-link">
+              Home
+            </li>
+            <li className="page-title">Contact</li>
+          </ul>
+        </div>
+      </header>
+
+      <Header></Header>
+
+      <div className="background-image"></div>
+    </div>
+  );
 };
 
 export default Contact;
