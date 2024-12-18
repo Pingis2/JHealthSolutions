455c50acc40f8c5d426d4436dc2331eae7748521
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-12-18 11:16:56 +0100

    Fixed - background image

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,14 +2,18 @@ import contactImage from "../assets/images/contact/contact-image.png";
 
 export const Contact = () => {
   return (
-    <section className="contact-page">
-      <img className="background-image" src={contactImage} alt="image of berlin"/>
+    <div className="contact-page">
+      <img
+        className="background-image"
+        src={contactImage}
+        alt="image of berlin"
+      />
       <div className="text-container">
         <h1>Kontakt</h1>
         <h2>Bitte senden Sie uns Ihre Nachricht an</h2>
         <p>jhealthsolutions.hd@gmail.com</p>
       </div>
-    </section>
+    </div>
   );
 };
 
