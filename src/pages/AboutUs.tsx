7ace931001f1fe30a1015a472246bf8fa1653adf
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-12-19 10:58:37 +0100

    Added - translate to about us page

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,52 +1,47 @@
+import { useTranslation } from "react-i18next";
 import image from "../assets/images/contact/contact-image.png";
 
 export const AboutUs = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="about-us-container">
       <img src={image} alt="image of berlin" className="background-image"></img>
       <section className="texts">
         <section className="text-1">
-          <p>
-            Angaben gemäß § 5 TMG (Telemediengesetz)<br></br>
-            JHealthSolutions GmbHG<br></br>
-            Bussemergasse 4<br></br>
-            69117 Heidelberg
-          </p>
-          <p>
-            Vertreten durch:<br></br>
-            Jessica Bräun
-          </p>
-          <p>
-            Kontakt:<br></br>
-            E-Mail: jhealthsolution.HD@gmail.com<br></br>
-            Eintragung im Handelsregister.<br></br>
-            Registergericht: Mannheim<br></br>
-            Registernummer: HRB 752149<br></br>
-            Steuernummer: 35002/90412<br></br>
-            Umsatzsteuer-Identifikationsnummer gemäß § 27 a<br></br>
-            Umsatzsteuergesetz: DE346601919
-          </p>
-          <p>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</p>
-          <p>
-            Jessica Bräun<br></br>
-            Bussemergasse 4<br></br>
-            69117 Heidelberg
-          </p>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: t("about-us.section1.company_info"),
+            }}
+          ></p>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: t("about-us.section1.represented_by"),
+            }}
+          ></p>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: t("about-us.section1.contact_info"),
+            }}
+          ></p>
+          <p>{t("about-us.section1.responsible_content")}</p>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: t("about-us.section1.responsible_person"),
+            }}
+          ></p>
         </section>
         <section className="text-2">
-          <p>
-            Hinweis zur Online-Streitbeilegung (OS):<br></br>
-            Die Europäische Kommission stellt eine Plattform zur
-            Online-Streitbeilegung (OS) bereit:
-            https://ec.europa.eu/consumers/odr Unsere E-Mail-Adresse finden Sie
-            oben im Impressum.
-          </p>
-          <p>
-            Haftungsausschluss (Disclaimer):<br></br>
-            Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für
-            die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können
-            wir jedoch keine Gewähr übernehmen.
-          </p>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: t("about-us.section2.online_dispute"),
+            }}
+          ></p>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: t("about-us.section2.disclaimer"),
+            }}
+          ></p>
         </section>
       </section>
     </div>
