df17ed35fb0ce788d8a4c02718dfe8a4c8fe1e7c
Pingis2 <anton@worksit.se>
2024-12-20 16:56:25 +0100

    added translation and most of the science page content

--- src/pages/ScienceTechnology.tsx
+++ src/pages/ScienceTechnology.tsx
@@ -1,5 +1,8 @@
+import { useTranslation } from "react-i18next";
 import scienceImage from "../assets/images/home-page/science-image.jpg";
 export const ScienceTechnology = () => {
+
+  const { t } = useTranslation();
 
   return (
     <>
@@ -11,6 +14,62 @@ export const ScienceTechnology = () => {
             className="science-technology-image"
           />
         </div>
+        <section className="text-info-section">
+          <h2>
+            {t("science-technology-page.headline")}
+          </h2>
+          <section className="operating-principle">
+            <h3>{t("science-technology-page.operating-principle.headline")}</h3>
+            <ul>
+              <li>
+                <p>{t("science-technology-page.operating-principle.phase-1")}</p>
+              </li>
+              <li>
+                <p>{t("science-technology-page.operating-principle.phase-2")}</p>
+              </li>
+              <li>
+                <p>{t("science-technology-page.operating-principle.phase-3")}</p>
+              </li>
+              <li>
+                <p>{t("science-technology-page.operating-principle.phase-4")}</p>
+              </li>
+              </ul>
+          </section>
+          <section className="technology">
+            <h3>{t("science-technology-page.technology.headline")}</h3>
+
+            <h4>{t("science-technology-page.technology.first-text.headline")}</h4>
+            <ul>
+              <li>
+                <p>{t("science-technology-page.technology.first-text.text-1")}</p>
+              </li>
+            </ul>
+
+            <h4>{t("science-technology-page.technology.first-text.headline")}</h4>
+            <ul>
+              <li>
+                <p>{t("science-technology-page.technology.second-text.text-1")}</p>
+              </li>
+              <li>
+                <p>{t("science-technology-page.technology.second-text.text-2")}</p>
+              </li>
+              <li>
+                <p>{t("science-technology-page.technology.second-text.text-3")}</p>
+              </li>
+              <li>
+                <p>{t("science-technology-page.technology.second-text.text-4")}</p>
+              </li>
+              <li>
+                <p>{t("science-technology-page.technology.second-text.text-5")}</p>
+              </li>
+              <li>
+                <p>{t("science-technology-page.technology.second-text.text-6")}</p>
+              </li>
+            </ul>
+
+          </section>
+          
+        </section>
       </div>
     </>
   );
